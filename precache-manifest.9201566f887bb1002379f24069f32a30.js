ed2cb49c4400d7acf7e6b2faae8cc0429dd39168
Platoniux <chizzza@gmail.com>
2019-01-06 22:11:42 +0200

    Updates

---
+++ precache-manifest.9201566f887bb1002379f24069f32a30.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9045f7e966c59b328c4b",
+    "url": "/ToDo-List/static/css/main.f31a2d8e.chunk.css"
+  },
+  {
+    "revision": "9045f7e966c59b328c4b",
+    "url": "/ToDo-List/static/js/main.9045f7e9.chunk.js"
+  },
+  {
+    "revision": "d6f98a5fcbad7ce974d4",
+    "url": "/ToDo-List/static/js/1.d6f98a5f.chunk.js"
+  },
+  {
+    "revision": "794dcdc1d42c6838c92c",
+    "url": "/ToDo-List/static/js/runtime~main.794dcdc1.js"
+  },
+  {
+    "revision": "2e0586ba1ff041973cd3dc391f3fda6c",
+    "url": "/ToDo-List/index.html"
+  }
+];
